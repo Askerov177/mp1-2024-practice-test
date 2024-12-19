eda7ea57e83051b664476dc06bdf847bb0e3df79
Askerov177 <askerovanar32@gmail.com>
2024-12-19 10:33:10 +0300

    Update task2.c

--- 3824Б1ПР4/02 Аскеров Анар Раминович/task2.c
+++ 3824Б1ПР4/02 Аскеров Анар Раминович/task2.c
@@ -1,15 +1,30 @@
-// Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
-// Найти индексы k1 и k2 двух элементов массива, 
-// расстояние |A[k1] – A[k2]| между которыми минимально.
-// Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
-// При невозможности найти пару, вернуть {-1,-1}.
-struct Indices
-{
-  int first_index;
-  int second_index;
+#include <stdio.h>
+#include <math.h> 
+
+struct Indices {
+    int first_index;
+    int second_index;
 };
-struct Indices task2(double A[], size_t n)
-{
-  struct Indices indices = {-2, -2};
-  return indices;
+
+struct Indices task2(double A[], size_t n) {
+    if (n < 2) {
+        return (struct Indices){-1, -1};
+    }
+
+    struct Indices indices = {0, 1}; 
+    double min_distance = fabs(A[0] - A[1]);
+
+    for (size_t i = 0; i < n - 1; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+            double current_distance = fabs(A[i] - A[j]);
+            if (current_distance < min_distance || 
+                (current_distance == min_distance && (int)i < indices.first_index)) {
+                min_distance = current_distance;
+                indices.first_index = (int)i;
+                indices.second_index = (int)j;
+            }
+        }
+    }
+
+    return indices;
 }
