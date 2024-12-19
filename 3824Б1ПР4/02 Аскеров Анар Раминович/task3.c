e232246fda67fb6652e657bc1539c3744561d9eb
Askerov177 <askerovanar32@gmail.com>
2024-12-19 10:35:08 +0300

    Update task3.c

--- 3824Б1ПР4/02 Аскеров Анар Раминович/task3.c
+++ 3824Б1ПР4/02 Аскеров Анар Раминович/task3.c
@@ -1,5 +1,18 @@
-// Задача 3. Дана строка s. Подсчитать количество содержащихся в ней цифр.
-size_t task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <ctype.h> 
+
+size_t task3(char *s) {
+    if (s == NULL) {
+        return 0; 
+    }
+
+    size_t count = 0; 
+
+    for (size_t i = 0; s[i] != '\0'; ++i) {
+        if (isdigit(s[i])) { 
+            count++;
+        }
+    }
+
+    return count; 
 }
